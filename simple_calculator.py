387de491dc98f838f4f196208020beb98cb5324a
Arpit Vikram Singh <139047299+ARPIT311095@users.noreply.github.com>
2026-01-02 02:52:18 -0800

    Update print statements to include operation type

--- simple_calculator.py
+++ simple_calculator.py
@@ -23,12 +23,12 @@ num1 = float(sys.argv[2])
 num2 = float(sys.argv[3])
 
 if choice == "1":
-    print("Result:", add(num1, num2))
+    print("Result:This is Add operation", add(num1, num2))
 elif choice == "2":
-    print("Result:", subtract(num1, num2))
+    print("Result:This is Sub operation", subtract(num1, num2))
 elif choice == "3":
-    print("Result:", multiply(num1, num2))
+    print("Result:This is Multiplecation operation", multiply(num1, num2))
 elif choice == "4":
-    print("Result:", divide(num1, num2))
+    print("Result:This is Division operation", divide(num1, num2))
 else:
     print("Invalid choice")
